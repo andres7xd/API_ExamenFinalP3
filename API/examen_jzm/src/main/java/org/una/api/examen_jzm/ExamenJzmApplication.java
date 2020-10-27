1a96f581dc45ae13acd7ff1aa91a1190087d26a6
andres7xd <58347580+andres7xd@users.noreply.github.com>
2020-10-26 22:55:35 -0600

    generacion del spring
    Se crea el spring para trabajar la base de datos

---
+++ API/examen_jzm/src/main/java/org/una/api/examen_jzm/ExamenJzmApplication.java
@@ -0,0 +1,13 @@
+package org.una.api.examen_jzm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenJzmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenJzmApplication.class, args);
+	}
+
+}
---
+++ API/examen_jzm/src/test/java/org/una/api/examen_jzm/ExamenJzmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.una.api.examen_jzm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenJzmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
